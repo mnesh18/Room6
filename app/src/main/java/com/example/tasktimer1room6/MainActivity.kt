c5d908e74f7457011809bbe41523fb222b117f57
mrwa94 <90597166+mrwa94@users.noreply.github.com>
2022-01-06 11:37:41 +0300

    edit UI and Activitys

--- app/src/main/java/com/example/tasktimer1room6/MainActivity.kt
+++ app/src/main/java/com/example/tasktimer1room6/MainActivity.kt
@@ -1,24 +1,27 @@
 package com.example.tasktimer1room6
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-// Muna branch
+      var addTask_btn = findViewById<Button>(R.id.btn_addTask)
+        addTask_btn.setOnClickListener {
+            val intent = Intent(this,AddActivity::class.java)
+            startActivity(intent)
+        }
+
+        var viewTask_btn = findViewById<Button>(R.id.btn_viewTask)
+        viewTask_btn.setOnClickListener {
+            val intent = Intent(this,ViewActivity::class.java)
+            startActivity(intent)
+        }
+
+
     }
-
-
-    //i am from basema system
-
-    //daad1
-// Muna Main
-
-//     test 7
-//    test9
-//    test 11
-//    Muna 3 Test 1
 }
